efb5d58a98fcf10f3e909fa8e994c215c83e66e0
alex4401 <rylatgl@gmail.com>
2020-11-30 21:17:13 +0100

    Add more gutter section types to Anhydrate

--- ReaperKing.Anhydrate/Models/LayoutInfo.cs
+++ ReaperKing.Anhydrate/Models/LayoutInfo.cs
@@ -3,12 +3,20 @@ namespace ReaperKing.Anhydrate.Models
     public struct GutterLayoutSection
     {
         public string Id { get; init; }
-        public ColumnType Type { get; init; }
+        public SectionType Type { get; init; }
+        public string CustomClass { get; init; }
+        public string HtmlId { get; init; }
 
-        public enum ColumnType
+        public enum SectionType
         {
             Full,
             Grid,
+            Row,
+            NarrowFit,
+            Narrower,
+            Break,
+            BreakMobile,
+            Custom,
         }
     }
 }
